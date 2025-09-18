14e6930b2a6c78405f08d1b3e63fb0eef1174ce6
deveshcse <d.mishra56603@gmail.com>
2025-09-18 22:56:59 +0530

    feat: implement DataTable, DataTablePagination, and TableSkeleton components for enhanced data handling and loading states

---
+++ src/components/dataTable/DataTable.jsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import {
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { flexRender } from "@tanstack/react-table";
+import { DataTablePagination } from "./DataTablePagination";
+import { Input } from "../ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { TableSkeleton } from "./TableSkeleton";
+
+export function DataTable({ data, columns, isLoading }) {
+  const [sorting, setSorting] = useState([]);
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+      rowSelection,
+      globalFilter,
+    },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setGlobalFilter,
+    onRowSelectionChange: setRowSelection,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    globalFilterFn: "includesString",
+    initialState: { pagination: { pageSize: 10 } },
+  });
+
+  return (
+    <div className="flex flex-col gap-1 px-4 py-2">
+      {/* Search */}
+      <div className="flex items-center gap-2">
+        <Input
+          placeholder="Search by title or author..."
+          value={globalFilter ?? ""}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          className="w-[200px]"
+        />
+      </div>
+
+      {/* Table */}
+      <div className="border rounded-lg h-[570px] overflow-auto">
+        <Table>
+          {/* Always render headers */}
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id} className="sm:min-w-24">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+
+          {/* Body switches between skeleton rows and real rows */}
+          <TableBody>
+            {isLoading ? (
+              <TableSkeleton table={table} />
+            ) : table.getRowModel().rows.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  className="hover:bg-gray-100 fade-in transition-all"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="text-center h-24"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+
+      {/* Pagination */}
+      {isLoading ? (
+        
+        <div className="flex items-center justify-end">
+          {/* Skeleton for Pagination buttons */}
+          <div className="flex gap-2">
+            <Skeleton className="h-8 w-8 rounded-md bg-gray-500" /> 
+            <Skeleton className="h-8 w-8 rounded-md bg-gray-500" /> 
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center justify-end">
+          <DataTablePagination table={table} />
+        </div>
+      )}
+    </div>
+  );
+}
